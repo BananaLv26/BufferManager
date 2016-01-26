2f607982d5cc3f0b02462a37d07d9209255d62dd
Pu Dong <pd22@water.clear.rice.edu>
2016-01-26 14:34:14 -0600

    init commit

---
+++ Main/BufferMgr/source/MyDB_PageHandle.cc
@@ -0,0 +1,23 @@
+
+#ifndef PAGE_HANDLE_C
+#define PAGE_HANDLE_C
+
+#include <memory>
+#include "MyDB_PageHandle.h"
+
+void *MyDB_PageHandleBase :: getBytes () {
+	return nullptr;
+}
+
+void MyDB_PageHandleBase :: wroteBytes () {
+}
+
+MyDB_PageHandleBase :: ~MyDB_PageHandleBase () {
+}
+
+MyDB_PageHandleBase :: MyDB_PageHandleBase(MyDB_BufferManager* my_pBM){
+	pBM = my_pBM;
+}
+
+#endif
+
